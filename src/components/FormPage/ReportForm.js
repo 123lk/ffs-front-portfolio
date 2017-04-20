a06c1659d4b44479844e7395c727fbc0cb71cef5
Laura Kenny <laurakenny@Lauras-MacBook-Air.local>
2017-04-20 16:03:03 +0100

    added form styling

--- src/components/FormPage/ReportForm.js
+++ src/components/FormPage/ReportForm.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class ReportForm extends React.Component {
-  render () {
+  render() {
     return (
-      <h1>This is a ReportForm</h1>
+      <div>
+        <form>
+          <div className="form-group">
+            <label>URL of potential fake news article</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>supporting information</label>
+            <textarea className="form-control" rows="8" />
+          </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+        </form>
+      </div>
     );
   }
 }
