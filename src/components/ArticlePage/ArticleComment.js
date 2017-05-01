707018e3f6327a0a8ef6d7d08dea566519df74a4
Laura Kenny <laurakenny@Lauras-MacBook-Air.local>
2017-05-01 12:23:01 +0100

    altered styling to vote arrows and comment card

--- src/components/ArticlePage/ArticleComment.js
+++ src/components/ArticlePage/ArticleComment.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import ArticleCommentForm from './ArticleCommentForm';
 
 class ArticleComment extends React.Component {
-  constructor () {
-    super ();
+  constructor() {
+    super();
     this.state = {
       showReply: false,
       showComments: false
@@ -12,18 +12,18 @@ class ArticleComment extends React.Component {
   }
   onClickReply(event) {
     event.preventDefault();
-    this.setState({showReply: !this.state.showReply});
+    this.setState({ showReply: !this.state.showReply });
   }
   onClickShowComments(event) {
     event.preventDefault();
-    this.setState({showComments: !this.state.showComments});
+    this.setState({ showComments: !this.state.showComments });
   }
   showComments(length) {
     if (length) return 'Show ' + length + ' comments';
   }
   render() {
     return (
-      <div className="container-fluid comment-card">
+      <div className="comment-card">
         <div className="container vote">
           <div className="glyphicon glyphicon-arrow-up" aria-hidden="true"></div>
           <div className="vote-count">{this.props.votes}</div>
@@ -32,11 +32,11 @@ class ArticleComment extends React.Component {
         <div className="rhs">
           <span className="comment-author"> {this.props.author}</span>
           <div> {this.props.comment}</div>
-          <a onClick= {this.onClickReply.bind(this)} type="button"><span>Reply</span></a>
+          <a onClick={this.onClickReply.bind(this)} type="button"><span>Reply</span></a>
           <div className="divider" />
-          <a onClick= {this.onClickShowComments.bind(this)} type="button"><span>{this.showComments(this.props.length)}</span></a>
+          <a onClick={this.onClickShowComments.bind(this)} type="button"><span>{this.showComments(this.props.length)}</span></a>
         </div>
-        {this.state.showReply && <ArticleCommentForm parentCommentId={this.props.parentCommentId}/>}
+        {this.state.showReply && <ArticleCommentForm parentCommentId={this.props.parentCommentId} />}
         {this.state.showComments && <div className="child-component">{this.props.renderComments(this.props.children)}</div>}
       </div>
     );
