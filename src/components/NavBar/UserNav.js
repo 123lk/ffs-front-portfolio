09878c9ab4f27c631a98051da4dd8566348d9094
Joe Caine <jlcaine1@gmail.com>
2017-05-01 14:04:04 +0100

    got initial styling done, and got navbar to talk to my algolia account (#14)

--- src/components/NavBar/UserNav.js
+++ src/components/NavBar/UserNav.js
@@ -1,25 +1,21 @@
 import React from 'react';
+import AgAutocomplete from 'react-algoliasearch';
+import '../../css/UserNav.scss';
 
 class UserNav extends React.Component {
   render() {
     return (
       <div className="user-nav">
         <a href="http://localhost:9090/"> <img src="http://1k6kcz5e21e3yunpze4q1cxn.wpengine.netdna-cdn.com/files/2015/02/ffs.jpg" alt="logo" /></a>
-        <div className="search-bar">
-          <div className="row">
-            <div className="col-md-12">
-              <div id="custom-search-input">
-                <div className="input-group col-md-12">
-                  <input type="text" className="form-control input-md" placeholder="Search..." />
-                  <span className="input-group-btn">
-                    <button className="btn btn-info btn-md" type="button">
-                      <i className="glyphicon glyphicon-search"></i>
-                    </button>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="algolia-search">
+          <AgAutocomplete
+            apiKey={'ecebdf5fdebcddc8c94f127d1eee4e4c'}
+            appId={'BSCGDWJBKU'}
+            displayKey='name'
+            indices={[{ index: 'FFS' }]}
+            inputId='input-search'
+            placeholder='Search...'
+          />
         </div>
         <div className="user-nav-box">
           <ul>
